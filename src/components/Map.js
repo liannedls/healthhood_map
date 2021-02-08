cbc943f045da44df2ca11f3444f4ead23d2422f6
liannedls <liannedelasalle@gmail.com>
2021-02-07 19:07:37 -0600

    added toggling markers map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import mapboxgl from "mapbox-gl";
 import Popup from "./Popup";
@@ -9,88 +9,112 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 const Map = () => {
   const mapContainerRef = useRef(null);
   const popUpRef = useRef(new mapboxgl.Popup({ offset: 15 }));
+  const [buttonref, setButtonref] = useState(true);
+  const [map, setMap] = useState(null);
 
   // initialize map when component mounts
+
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       // See style options here: https://docs.mapbox.com/api/maps/#styles
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [            -75.65494537353516,
-        45.43267179295816],
+      center: [-75.65494537353516, 45.43267179295816],
       zoom: 11,
-      maxBounds:[
+      maxBounds: [
         [-76.072173, 45.052915],
-        [-75.289848, 45.673931]
-      ]
+        [-75.289848, 45.673931],
+      ],
     });
-
+    setMap(map);
     // add navigation control (zoom buttons)
     map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
 
-    map.on("load", () => {
-      // add the data source for new a feature collection with no features
-      map.addSource("random-points-data", {
-        type: "geojson",
-        data: GeoJsonVal
-        
+    if (buttonref) {
+      console.log("addmap effect called");
+      console.log(buttonref);
+
+      map.on("load", () => {
+        // add the data source for new a feature collection with no features
+        map.addSource("random-points-data", {
+          type: "geojson",
+          data: GeoJsonVal,
+        });
+        // now add the layer, and reference the data source above by name
+        map.addLayer({
+          id: "random-points-layer",
+          source: "random-points-data",
+          type: "symbol",
+          layout: {
+            // full list of icons here: https://labs.mapbox.com/maki-icons
+            "icon-image": "bakery-15", // this will put little croissants on our map
+            "icon-padding": 0,
+            "icon-allow-overlap": true,
+            visibility: "visible",
+          },
+        });
       });
-      // now add the layer, and reference the data source above by name
-      map.addLayer({
-        id: "random-points-layer",
-        source: "random-points-data",
-        type: "symbol",
-        layout: {
-          // full list of icons here: https://labs.mapbox.com/maki-icons
-          "icon-image": "bakery-15", // this will put little croissants on our map
-          "icon-padding": 0,
-          "icon-allow-overlap": true
+
+      map.on("moveend", async () => {
+        // get new center coordinates
+        const { lng, lat } = map.getCenter();
+        // fetch new data
+        //const results = await fetchFakeData({ longitude: lng, latitude: lat });
+        // update "random-points-data" source with new data
+        // all layers that consume the "random-points-data" data source will be updated automatically
+        map.getSource("random-points-data").setData(GeoJsonVal);
+      });
+
+      // change cursor to pointer when user hovers over a clickable feature
+      map.on("mouseenter", "random-points-layer", (e) => {
+        if (e.features.length) {
+          map.getCanvas().style.cursor = "pointer";
         }
       });
-    });
 
-    map.on("moveend", async () => {
-      // get new center coordinates
-      const { lng, lat } = map.getCenter();
-      // fetch new data
-      //const results = await fetchFakeData({ longitude: lng, latitude: lat });
-      // update "random-points-data" source with new data
-      // all layers that consume the "random-points-data" data source will be updated automatically
-      map.getSource("random-points-data").setData(GeoJsonVal);
-    });
+      // reset cursor to default when user is no longer hovering over a clickable feature
+      map.on("mouseleave", "random-points-layer", () => {
+        map.getCanvas().style.cursor = "";
+      });
 
-    // change cursor to pointer when user hovers over a clickable feature
-    map.on("mouseenter", "random-points-layer", e => {
-      if (e.features.length) {
-        map.getCanvas().style.cursor = "pointer";
-      }
-    });
-
-    // reset cursor to default when user is no longer hovering over a clickable feature
-    map.on("mouseleave", "random-points-layer", () => {
-      map.getCanvas().style.cursor = "";
-    });
-
-    // add popup when user clicks a point
-    map.on("click", "random-points-layer", e => {
-      if (e.features.length) {
-        const feature = e.features[0];
-        // create popup node
-        const popupNode = document.createElement("div");
-        ReactDOM.render(<Popup feature={feature} />, popupNode);
-        // set popup on map
-        popUpRef.current
-          .setLngLat(feature.geometry.coordinates)
-          .setDOMContent(popupNode)
-          .addTo(map);
-      }
-    });
-
+      // add popup when user clicks a point
+      map.on("click", "random-points-layer", (e) => {
+        if (e.features.length) {
+          const feature = e.features[0];
+          // create popup node
+          const popupNode = document.createElement("div");
+          ReactDOM.render(<Popup feature={feature} />, popupNode);
+          // set popup on map
+          popUpRef.current
+            .setLngLat(feature.geometry.coordinates)
+            .setDOMContent(popupNode)
+            .addTo(map);
+        }
+      });
+    }
     // clean up on unmount
     return () => map.remove();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []);
 
-  return <div className="map-container" ref={mapContainerRef} />;
+  function makeinvisible() {
+    //setButtonref(!buttonref);
+    console.log(map);
+    if (buttonref) {
+      map.setLayoutProperty("random-points-layer", "visibility", "none");
+      setButtonref(false);
+    } else {
+      map.setLayoutProperty("random-points-layer", "visibility", "visible");
+      setButtonref(true);
+    }
+  }
+
+  return (
+    <div className="map-container" ref={mapContainerRef}>
+      <button className="mgl-map-overlay " onClick={makeinvisible}>
+        Button
+      </button>
+    </div>
+  );
 };
 
-export default Map;
+export default React.memo(Map);
