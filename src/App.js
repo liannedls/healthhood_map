cf22ef19710fa9ef9137581861820ed7a7c8f4b7
liannedls <liannedelasalle@gmail.com>
2021-02-09 14:35:41 -0600

    package

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Map from './components/Map';
-import TopNav from './components/Navbar'
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from './components/Home';
-import SubmitLoc from './components/SubmitLoc.jsx';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Map from "./components/Map";
+import TopNav from "./components/Navbar";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import SubmitLoc from "./components/SubmitLoc.jsx";
+import Online from "./components/OnlineResources.js";
 
 export default function App() {
-    return (
-        <Router>
-        <div>
-          <h2>Welcome to React Router Tutorial</h2>
- <TopNav />
-          <hr />
-          <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/Map' component={Map} />
-              <Route path='/Submit' component={SubmitLoc} />
-          </Switch>
-        </div>
-      </Router>
-
-            )
+  return (
+    <Router>
+      <div>
+        <h2>Welcome to React Router Tutorial</h2>
+        <TopNav />
+        <hr />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/Map" component={Map} />
+          <Route path="/Online" component={Online} />
+          <Route path="/Submit" component={SubmitLoc} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
